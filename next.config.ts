028245ac651da5b29916716b52933a4c440416ea
sharif57 <sharifmahamud577951@gmail.com>
2025-03-10 16:47:57 +0600

    refactor next.config.ts to simplify configuration and remove unused code

--- next.config.ts
+++ next.config.ts
@@ -1,25 +1,15 @@
-// import type { NextConfig } from "next";
-
-// const nextConfig: NextConfig = {
-//   /* config options here */
-//   eslint: {
-//     ignoreDuringBuilds: true,
-//   },
-
-// };
-
-// export default nextConfig;
-
 import type { NextConfig } from "next";
-const withTM = require("next-transpile-modules")(["@ant-design/icons-svg"]); // Add this line
 
-const nextConfig: NextConfig = withTM({
+const nextConfig: NextConfig = {
+  /* config options here */
   eslint: {
     ignoreDuringBuilds: true,
   },
 
-  // Other config options
-  reactStrictMode: true, // Add strict mode for React
-});
+ 
+
+
+};
 
 export default nextConfig;
+
