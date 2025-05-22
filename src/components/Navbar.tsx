cde1824179aec4ad79052edbf25293ed5230a508
sharif57 <sharifmahamud577951@gmail.com>
2025-05-22 17:17:03 +0600

    fix: Comment out Link to my workout plan in Navbar component

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -413,14 +413,14 @@ export default function Navbar() {
           {userProfile ? (
             <div className="flex items-center gap-4">
               {/* Notifications */}
-              <Link href="/myworkoutplan">
+              {/* <Link href="/myworkoutplan">
                 <div className="border border-gray-300 rounded-full relative size-12 flex items-center justify-center">
                   <Bell size={28} className="text-black" />
                   <span className="absolute top-1 right-1 bg-[#012A60] text-white text-xs font-bold w-5 h-5 flex items-center justify-center rounded-full">
                     {workPlans.length}
                   </span>
                 </div>
-              </Link>
+              </Link> */}
               {/* Profile Dropdown */}
               <Dropdown menu={{ items }} placement="bottomRight" arrow>
                 <div className="flex items-center gap-3 cursor-pointer">
